8baa879a758892ec8db10f37f87735853e4f1016
Felipe Augusto <augusto.felipedev@gmail.com>
2026-08-20 12:11:47 -0300

    test(setup): mock dialog showModal and close for jsdom

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -5,3 +5,14 @@ import '@testing-library/jest-dom/vitest'
 afterEach(() => {
   cleanup()
 })
+
+// jsdom não implementa showModal/close nativamente
+// mock que reproduz o comportamento que os componentes dependem.
+HTMLDialogElement.prototype.showModal = function () {
+  this.setAttribute('open', '')
+}
+
+HTMLDialogElement.prototype.close = function () {
+  this.removeAttribute('open')
+  this.dispatchEvent(new Event('close'))
+}
